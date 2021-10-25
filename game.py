9d3c23f3d56d536b4806f63b98695ceeccfc7fbc
Russ Ford <russford@gmail.com>
2021-10-25 09:32:34 -0500

    map drawing with sprites

--- game.py
+++ game.py
@@ -10,7 +10,8 @@ pygame.init()
 director = director.Director()
 director.load_file("assets/map.txt")
 
-map_scene = scene_editor.SceneEditor(director)
+# map_scene = scene_editor.SceneEditor(director)
+map_scene = scene_map.SceneMap(director)
 director.set_scene(map_scene)
 
 director.loop()
--- scene_map.py
+++ scene_map.py
@@ -18,31 +18,29 @@ move_dict = {K_UP: (0,-1), K_DOWN: (0,1), K_LEFT: (-1,0), K_RIGHT: (1,0)}
 class SceneMap (scene.Scene):
     def __init__(self, director):
         scene.Scene.__init__(self, director)
-        self.block = 0
+        self.map_x, self.map_y = 0, 0
+        self.block = 24
+        self.spritesheet = utils.SpriteSheet("assets/colored_packed.png", 16, 16)
 
     def on_display(self, screen):
         screen.fill((0, 0, 0))
 
-        self.block = min(screen.get_rect().w // self.director.map_w, screen.get_rect().h // self.director.map_h)
-
-        for i in range(self.director.map_w):
-            for j in range(self.director.map_h):
-                if (i, j) in self.director.worldmap.map_dict:
-                    pygame.draw.rect(screen, (128, 128, 128), self.map_rect(i, j))
-
-        pygame.draw.rect(screen, (255, 0, 0), self.map_rect(*self.director.player.pos))
-
-        for pos, m in self.director.monsters.items():
-            if m.hp > 0:
-                pygame.draw.rect(screen, (0, 255, 0), self.map_rect(*pos))
+        all_players = self.director.monsters | {self.director.player.pos: self.director.player}
+        for i in range(self.block):
+            for j in range(self.block):
+                block_spot = (i + self.map_x, j + self.map_y)
+                if block_spot in self.director.worldmap.map_dict:
+                    screen.blit(self.spritesheet.img_at(*self.director.worldmap[block_spot]), self.map_rect(i, j))
+                if block_spot in all_players.keys():
+                    if all_players[block_spot].hp > 0:
+                        screen.blit(self.spritesheet.img_at(*all_players[block_spot].sprite_pos), self.map_rect(i, j))
 
     def on_event(self, event):
         if event.type == KEYDOWN:
-            if event.key in move_dict.keys():
-                direction = move_dict[event.key]
-                new_pos = self.director.player.pos[0] + direction[0], self.director.player.pos[1] + direction[1]
-                if new_pos not in self.director.worldmap.map_dict:
-                    self.director.player.pos = new_pos
+            new_pos = (self.director.player.pos[0] + (-1 if event.key == K_LEFT else 0) + (1 if event.key == K_RIGHT else 0),
+                       self.director.player.pos[1] + (-1 if event.key == K_UP else 0) + (1 if event.key == K_DOWN else 0))
+            if new_pos not in self.director.worldmap.map_dict:
+                self.director.player.pos = new_pos
 
     def on_update(self):
         for pos, m in self.director.monsters.items():
